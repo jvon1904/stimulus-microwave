b67f946ef295b4c1da8bb487bc7d8da51ff939e8
Jesse vonBergen <jvon1904@gmail.com>
2024-03-10 15:31:49 -0400

    paint door canvas

--- src/controllers/door_controller.js
+++ src/controllers/door_controller.js
@@ -25,15 +25,27 @@ export default class extends Controller {
     this.canvasTarget.width = this.paneTarget.offsetWidth;
     this.canvasTarget.height = this.paneTarget.offsetHeight;
     const ctx = this.canvasTarget.getContext("2d");
-    const grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 20);
-    // grd.addColorStop(1, "blue");
-    grd.addColorStop(0.5, "red");
 
-    // Draw a filled Rectangle
-    ctx.fillStyle = grd;
-    ctx.beginPath();
-    ctx.arc((this.canvasTarget.width / 2), (this.canvasTarget.height / 2) + 50, 100, 0, 2 * Math.PI);
-    ctx.fill();
-
+    // Paint the window with little circles
+    let r = 3.5; // radius
+    let d = 2 * r; // diameter
+    let gap = 1.5; // gap between circles
+    let w = d + gap; // total width
+    let marginX = 15; // pixels of space left and right
+    let marginY = 25; // pixels of space top and bottom
+    let numX = (this.canvasTarget.width / w) - ((marginX * 2) / w); // number of circles accross
+    let numY = (this.canvasTarget.height / w) - ((marginY * 2) / w); // number of circles down
+    ctx.strokeStyle = 'black'
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.13)'
+    for(let i = 0; i < numX; i++) {
+      let x = ((w) * i) + marginX;
+      for(let j = 0; j < numY; j++) {
+        let y = ((w) * j) + marginY;
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+        ctx.stroke();
+        ctx.fill();
+      }
+    }
   }
 }
