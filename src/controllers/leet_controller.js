aef7e598f45c56dfd33bf4712e8765a0cff659f2
Jesse vonBergen <jvon1904@gmail.com>
2024-03-31 19:36:40 -0400

    create LEET canvas display

--- src/controllers/leet_controller.js
+++ src/controllers/leet_controller.js
@@ -1,22 +1,67 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  beepEffect = new Audio("leet-beep.mp3");
 
   static targets = [
-    "outline"
+    "outline", "screen", "canvas"
   ]
 
   static values = {
     originalBackground: String
   }
 
+  beep() {
+    this.beepEffect.currentTime = 0;
+    this.beepEffect.play();
+  }
+
+
   start() {
     this.backgroundValue = this.outlineTarget.style.backgroundImage;
-    this.outlineTarget.style.background = this.randomGrad;
+    this.showScreen();
+    this.display('Welcome to LEET mode...');
   }
 
   stop() {
     this.outlineTarget.style.background = this.originalBackgroundValue;
+    this.hideScreen();
+  }
+
+  showScreen() {
+    this.screenTarget.classList.add('on');
+  }
+
+  hideScreen() {
+    this.screenTarget.classList.remove('on');
+  }
+
+  setColor() {
+    this.outlineTarget.style.background = this.randomGrad;
+  }
+
+  display(txt = 'Hello world') {
+    this.canvasTarget.width = this.canvasTarget.offsetWidth * this.pixelRatio;
+    this.canvasTarget.height = this.canvasTarget.offsetHeight * this.pixelRatio;
+    
+    const ctx = this.canvasTarget.getContext("2d");
+    const chars = txt.split('');
+
+    ctx.font = "24px monospace";
+    ctx.textAlign = "start";
+    ctx.textBaseline = "bottom";
+    ctx.fillStyle = "yellowgreen";
+
+    let disp = [];
+    for (let i = 0; i < chars.length; i++) {
+      setTimeout(() => {
+        disp.push(chars[i]);
+        if (i % 2 === 0) {
+          this.beep();
+        }
+        ctx.fillText(disp.join(''), 10, 50);
+      }, 50 * i);
+    }
   }
 
   get randomGrad() {
@@ -25,5 +70,9 @@ export default class extends Controller {
     const col2 = `hsl(${col}, 100%, 33%)`;
     const col3 = `hsl(${col}, 100%, 20%)`;
     return `linear-gradient(210deg, ${col1}, ${col2} 25%, ${col3} 80%)`
+  }
+
+  get pixelRatio() {
+    return window.devicePixelRatio;
   }
 }
